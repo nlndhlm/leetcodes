c554798067aa966d383fd45b8e825519f7fa66e6
nlndhlm <30705699+nlndhlm@users.noreply.github.com>
2022-08-30 23:04:19 +0200

    Add files via upload

---
+++ 13 - roman to integer - accepted.py
@@ -0,0 +1,28 @@
+# Leetcode 13 - Roman to Integer
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        # Lager en dict med korresponderende verdier:
+        dict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+
+        t = []
+
+        # Vi mapper rett verdi til rett romertall og legger i lista:
+        for letter in s:
+            t.append(dict[letter])
+
+        # Sammenligner størrelse på hvert tall med det neste tallet i lista:
+        u = 0
+
+        while u < len(t) - 1:
+            if t[u] < t[u + 1]:
+                t[u] = t[u] * (-1) # multipliserer med -1 dersom tallet er mindre enn det neste
+
+            u = u + 1
+
+        return sum(t)
